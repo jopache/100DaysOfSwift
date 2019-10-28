64249f082212f99baadee9fd024e9d5c5cc78e21
Jose-Pacheco <jose.pacheco@twinspires.com>
2019-10-27 22:52:02 -0400

    Add a details screen showing image and minor enhancements

---
+++ StormViewer/StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Jose Pacheco on 10/27/19.
+//  Copyright © 2019 jopache. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
